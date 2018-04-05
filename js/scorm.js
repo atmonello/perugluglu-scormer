7b65a813dd7b9ad472e5a699893dc52cccee86fb
Andre Monello <atmonello@outlook.com>
2018-04-05 14:36:49 -0300

    Added credits to the files.

--- js/scorm.js
+++ js/scorm.js
@@ -1,3 +1,17 @@
+/*
+** Perugluglu SCORM 1.2 Wrapper for HTML5 games
+** Author: Andre Monello @Perugluglu Interactive @Sao Paulo, Brasil
+**
+** This XML can be used to integrate SCORM into your HTML5 games, 
+** along with the HTML, JS and any other files.Project can be
+** imported to SCORM Cloud platform.
+**
+** It uses Pipwerks SCORM Wrapper for JavaScript
+** https://github.com/pipwerks/scorm-api-wrapper 
+*/
+
+
+
 var scorm = pipwerks.SCORM; // Used to access Pipwerks SCORM Wrapper
 var lmsConnected = false; // Checks if coonected to the LMS
 var lessonStatus; // Stores course status (from LMS)
--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+/*
+** Perugluglu SCORM 1.2 Wrapper for HTML5 games
+** Author: Andre Monello @Perugluglu Interactive @Sao Paulo, Brasil
+**
+** This XML can be used to integrate SCORM into your HTML5 games, 
+** along with the HTML, JS and any other files.Project can be
+** imported to SCORM Cloud platform.
+**
+** It uses Pipwerks SCORM Wrapper for JavaScript
+** https://github.com/pipwerks/scorm-api-wrapper 
+*/
+
+
 scormInit();
 
 function init() {
